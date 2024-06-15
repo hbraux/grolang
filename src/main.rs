0953ed2accca4ed56341d1ee2e9953b0078d1fc9
Harold.Braux <harold.braux@bleckwen.ai>
2024-06-15 15:33:48 +0200

    add rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::{env, io};
 use std::collections::HashMap;
+use std::io::Write;
 
 use parser::{eval_expr, read_expr};
 
-static VERSION: &str = "0.1";
+const VERSION: &str = "0.1";
+const PROMPT: &str  = "> ";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.is_empty() {
+    if args.len() <= 1 {
         repl()
     } else {
         match args[1].as_ref() {
@@ -19,17 +21,27 @@ fn main() {
 
 fn repl() {
     println!("Bienvenue sur groLang version {}", VERSION);
-    println!("Taper :q pour quiier, :h pour de l'aide");
+    println!("Taper :q pour quitter, :h pour de l'aide");
     let values: HashMap<&str, i64> = HashMap::new();
     loop {
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
+        print!("{}", PROMPT);
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        match io::stdin().read_line(&mut line) {
             Err(_) => break,
             _ => {},
         }
-        let expr = read_expr(&input);
+        let input = line.trim();
+        if input.starts_with(':') {
+            match input {
+                ":q" => break,
+                _ => continue,
+            }
+        }
+        let expr = read_expr(input);
         let result = eval_expr(expr, &values);
         println!("{}", result)
     }
+    println!(".")
 }
 
