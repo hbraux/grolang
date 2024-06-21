3b2ed64e09eb89f79862f36bf06f1dc461624034
Harold.Braux <harold.braux@bleckwen.ai>
2024-06-21 16:03:39 +0200

    add types

--- src/lib.rs
+++ src/lib.rs
@@ -4,36 +4,50 @@ lalrpop_mod!(pub grammar);
 
 use std::collections::HashMap;
 use std::string::ToString;
+use strum_macros::Display;
 use ErrorType::{DivisionByZero, NotANumber, UndefinedSymbol, CannotParse};
 use Expr::{Bool, Declare, Error, Float, Id, Int, Null, BinaryOp, Str};
+use crate::Expr::{TypeSpec, TypeExpr};
 
 
-#[derive(Debug, Eq, PartialEq, Clone)]
-pub enum Type {
+#[derive(Debug, Eq, PartialEq, Clone, Display)]
+pub enum ClassType {
     Any,
     Int,
     Bool,
+    Str,
+    Float,
+    Error,
     Custom(String),
-    List(Box<Type>),
-    Map(Box<Type>,Box<Type>)
+    List(Box<ClassType>),
+    Option(Box<ClassType>),
+    Fail(Box<ClassType>),
+    Map(Box<ClassType>, Box<ClassType>)
 }
 
-impl Type {
-    pub fn new(str: &str) -> Type {
-        if str.starts_with("List<") {
-            Type::List(Box::new(Type::new(&str[5..str.len()-1])))
+impl ClassType {
+    pub fn new(str: &str) -> ClassType {
+        if str.ends_with("?") {
+            ClassType::Option(Box::new(ClassType::new(&str[0..str.len()-1])))
+        } else if str.ends_with("!") {
+            ClassType::Fail(Box::new(ClassType::new(&str[0..str.len()-1])))
+        } else if str.starts_with("List<") {
+            ClassType::List(Box::new(ClassType::new(&str[5..str.len() - 1])))
+        }else if str.starts_with("List<") {
+            ClassType::List(Box::new(ClassType::new(&str[5..str.len()-1])))
         } else if str.starts_with("Map<") {
             let s : Vec<&str> = (&str[4..str.len()-1]).split(',').collect();
-            Type::Map(Box::new(Type::new(s[0])), Box::new(Type::new(s[1])))
+            ClassType::Map(Box::new(ClassType::new(s[0])), Box::new(ClassType::new(s[1])))
         } else {
             match str {
-                "Any" => Type::Any,
-                "Int" => Type::Int,
-                "Bool" => Type::Bool,
+                "Int" => ClassType::Int,
+                "Bool" => ClassType::Bool,
+                "Str" => ClassType::Str,
+                "Float" => ClassType::Float,
                 _ => if str.chars().all(|x| x.is_alphabetic()) {
-                    Type::Custom(str.to_string())
+                    ClassType::Custom(str.to_string())
                 } else {
-                    panic!("{} is not a valid type name", str)
+                    ClassType::Error
                 }
             }
         }
@@ -41,24 +55,25 @@ impl Type {
 }
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum ErrorType {
     DivisionByZero,
     UndefinedSymbol(String),
     CannotParse(String),
-    NotANumber
+    NotANumber,
+    InconsistentType(String)
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
     Int(i64),
     Float(f64),
     Str(String),
     Bool(bool),
     Id(String),
-    SimpleType(String),
-    ComplexType(String, Vec<String>),
-    Declare(String, Box<Expr>),
+    TypeSpec(String),
+    TypeExpr(ClassType),
+    Declare(String, Option<String>, Box<Expr>),
     BinaryOp(Box<Expr>, Opcode, Box<Expr>),
     Call(Box<Expr>, String, Vec<Box<Expr>>),
     Error(ErrorType),
@@ -69,12 +84,20 @@ pub const TRUE: Expr = Bool(true);
 pub const FALSE: Expr = Bool(false);
 pub const NULL: Expr = Null;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Opcode {
     Mul,
     Div,
     Add,
     Sub,
+    Mod,
+    Eq,
+    Neq,
+    Gt,
+    Ge,
+    Lt,
+    Le,
+    In
 }
 
 
@@ -87,13 +110,41 @@ impl Expr {
         }
     }
 
+    pub fn get_type(&self) -> ClassType {
+        match self {
+            Bool(_) => ClassType::Bool,
+            Int(_) => ClassType::Int,
+            Float(_) => ClassType::Int,
+            Str(x) => ClassType::Str,
+            _ => ClassType::Any
+        }
+    }
+
+    pub fn is_error(&self) -> bool {
+        matches!(self, Error(_))
+    }
+
+
     pub fn eval(self, ctx: &mut Context) -> Expr {
         match self {
             Id(s) => ctx.get(&*s).clone(),
-            Declare(s, right) => { let value = right.eval(ctx); ctx.set(s.as_str(), value) },
+            TypeSpec(s) => TypeExpr(ClassType::new(s.as_str())),
+            Declare(s, spec, right) => self.declare(s, spec, right.eval(ctx));
             BinaryOp(left, code, right) => eval_op(code, left.eval(ctx), right.eval(ctx)),
             _ => self.clone()
         }
+    }
+
+    fn declare(&self, name: String, spec: String, value: Expr) -> Expr {
+        if !value.is_error() {
+            ctx.set(name.as_str(), value.clone());
+            if let Some(expected) = spec {
+                if value.get_type() != ClassType::new(s.as_str()) {
+                    return Error(ErrorType::InconsistentType(value.get_type().to_string()))
+                }
+            }
+        }
+        value
     }
 
     pub fn print(self) -> String {
@@ -122,9 +173,8 @@ impl Context {
             None => Error(UndefinedSymbol(name.to_string())),
         }
     }
-    pub fn set(&mut self, name: &str, expr: Expr) -> Expr {
+    pub fn set(&mut self, name: &str, expr: Expr) {
         self.values.insert(name.to_string(), expr);
-        NULL
     }
 
 }
@@ -136,6 +186,7 @@ fn eval_op(code: Opcode, left: Expr, right: Expr) -> Expr {
             Opcode::Sub => Int(a - b),
             Opcode::Mul => Int(a * b),
             Opcode::Div => if b !=0 { Int(a / b) } else { Error(DivisionByZero) },
+            _ => panic!()
         }
     } else {
         Error(NotANumber)
@@ -144,9 +195,11 @@ fn eval_op(code: Opcode, left: Expr, right: Expr) -> Expr {
 
 #[test]
 fn test() {
-    assert_eq!(Type::Any, Type::new("Any"));
-    assert_eq!(Type::Int, Type::new("Int"));
-    assert_eq!(Type::List(Box::new(Type::Int)), Type::new("List<Int>"));
-    assert_eq!(Type::Map(Box::new(Type::Int),Box::new(Type::Bool)), Type::new("Map<Int,Bool>"));
+    assert_eq!(ClassType::Any, ClassType::new("Any"));
+    assert_eq!(ClassType::Int, ClassType::new("Int"));
+    assert_eq!(ClassType::List(Box::new(ClassType::Int)), ClassType::new("List<Int>"));
+    assert_eq!(ClassType::Map(Box::new(ClassType::Int), Box::new(ClassType::Bool)), ClassType::new("Map<Int,Bool>"));
+    assert_eq!(ClassType::Option(Box::new(ClassType::Int)), ClassType::new("Int?"));
+    assert_eq!(ClassType::Fail(Box::new(ClassType::Int)), ClassType::new("Int!"));
 }
 
