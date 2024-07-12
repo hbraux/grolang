5e8d211aba69f80a132e2a74ffcb0c7ec39ee936
Harold.Braux <harold.braux@gmail.com>
2024-07-12 16:50:45 +0200

    add lambdas

--- src/lambda.rs
+++ src/lambda.rs
@@ -16,7 +16,7 @@ impl Clone for Lambda {
     fn clone(&self) -> Self { unreachable!() }
 }
 impl Debug for Lambda {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result { todo!() }
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result { unreachable!() }
 }
 impl PartialEq for Lambda {
     fn eq(&self, _other: &Self) -> bool { unreachable!() }
